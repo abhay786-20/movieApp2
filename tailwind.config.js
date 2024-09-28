a1f0770775c125f4988c9de06da7ec60a3e4e4ff
ABHAYSHARMA <as7766700@gmail.com>
2024-09-28 20:44:40 +0530

    feat:Initial Setup, using tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",    // file paths that needs to be using the CSS
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
